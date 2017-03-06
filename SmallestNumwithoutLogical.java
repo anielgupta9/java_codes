62e52cff9cc6b5feb45a5e12e94e160036bb3b28
anielgupta9 <anielgupta9@gmail.com>
2017-03-06 22:47:05 +0545

    SmallestNumwithoutLogical.java eddited

--- SmallestNumwithoutLogical.java
+++ SmallestNumwithoutLogical.java
@@ -1,6 +1,6 @@
 public class SmallestNumwithoutLogical { 
 	public static void main(String args[]) {
 		int num1 = 2, num2 = 3, num3 = 10;      
-		System.out.println("Largent no. is " + Math.max(num3, Math.max(num1, num2)));
+		System.out.println("Largent no. is " + Math.min(num3, Math.min(num1, num2)));
 	}
 }
