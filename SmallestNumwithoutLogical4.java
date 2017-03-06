962f17da9de084f669e3e3a33955edd1f4546777
krishnachalise418 <krishnachalise418@gmail.com>
2017-03-06 22:49:48 +0545

    GreatestNumwithoutLogical4.java added

---
+++ SmallestNumwithoutLogical4.java
@@ -0,0 +1,6 @@
+public class SmallestNumwithoutLogical4 { 
+	public static void main(String args[]) {
+		int num1 = 2, num2 = 3, num3 = 10, num4 = 15;      
+		System.out.println("Largent no. is " + Math.max(num4, Math.max(num3, Math.max(num1, num2))));
+	}
+}
