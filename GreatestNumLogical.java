dcac6965d9fd72ed9fd71338ffe344553ee0af2a
anielgupta9 <anielgupta9@gmail.com>
2017-03-06 21:56:57 +0545

    GreatestNumLogical.java edited

--- GreatestNumLogical.java
+++ GreatestNumLogical.java
@@ -2,7 +2,7 @@ public class GreatestNumLogical{
 	public static void main(String() args){
 		int num1 = 2, num2 = 3, num3 = 9;
 		if(num1 < num3 && num2 <num3){
-			System.out.println(num3 + "is greates");
+			System.out.println(num3 + "is greatest");
 		}
 		else if(num1 > num2 && num1 >num3){
 			System.out.println(num1 + "is greatest");
